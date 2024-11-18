de1918ecd9d4d4839775e552cb82fa9f5aa7209c
jthoeun <johnny.thoeun@gmail.com>
2024-11-17 19:53:54 -0700

    Learning Activity 1 commits

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 /* ***********************
  * View Engine and Template
  *************************/
@@ -23,9 +24,9 @@ app.set("layout", "./layouts/layout")//not at view point
  *************************/
 app.use(static)
 //Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
